d9b4a35101a35a1c2618cecd1e7009b355c8e976
siege <70180119+n0tsiege@users.noreply.github.com>
2021-09-11 15:40:56 -0400

    feat(config/languages): Updated config to english

--- config.lua
+++ config.lua
@@ -1,81 +1,71 @@
 Config = {}
 
-----------------------------------------------------
--------- Intervalles en secondes -------------------
-----------------------------------------------------
+--------------------------------------------
+-- Intervals in Seconds
+--------------------------------------------
 
--- Temps d'attente Antispam / Waiting time for antispam
+-- Waiting time for antispam
 Config.AntiSpamTimer = 2
 
--- Vérification et attribution d'une place libre / Verification and allocation of a free place
+-- Verification and allocation of a free place
 Config.TimerCheckPlaces = 3
 
--- Mise à jour du message (emojis) et accès à la place libérée pour l'heureux élu / Update of the message (emojis) and access to the free place for the lucky one
+-- Update of the message (emojis) and access to the free place for the lucky one
 Config.TimerRefreshClient = 3
 
--- Mise à jour du nombre de points / Number of points updating
+-- Number of points updating
 Config.TimerUpdatePoints = 6
 
-----------------------------------------------------
------------- Nombres de points ---------------------
-----------------------------------------------------
+--------------------------------------------
+-- Number of Points
+--------------------------------------------
 
--- Nombre de points gagnés pour ceux qui attendent / Number of points earned for those who are waiting
+-- Number of points earned for those who are waiting
 Config.AddPoints = 1
 
--- Nombre de points perdus pour ceux qui sont entrés dans le serveur / Number of points lost for those who entered the server
+-- Number of points lost for those who entered the server
 Config.RemovePoints = 1
 
--- Nombre de points gagnés pour ceux qui ont 3 emojis identiques (loterie) / Number of points earned for those who have 3 identical emojis (lottery)
-Config.LoterieBonusPoints = 25
+-- Number of points earned for those who have 3 identical emojis (lottery)
+Config.LoterieBonusPoints = 100
 
--- Accès prioritaires / Priority access
+-- Priority access
 Config.Points = {
 	-- {'steamID', points},
 	-- {'steam:0123456789', 1000}
 }
 
-----------------------------------------------------
-------------- Textes des messages ------------------
-----------------------------------------------------
+--------------------------------------------
+--Messages
+--------------------------------------------
 
--- Si steam n'est pas détecté / If steam is not detected
-Config.NoSteam = "Steam n'a pas été détecté. Veuillez (re)lancer Steam et FiveM, puis réessayer."
--- Config.NoSteam = "Steam was not detected. Please (re)launch Steam and FiveM, and try again."
+-- If steam is not detected
+Config.NoSteam = "Steam was not detected. Please (re)launch Steam and FiveM, and try again."
 
--- Message d'attente / Waiting text
-Config.EnRoute = "Vous êtes en route. Vous avez déjà parcouru"
--- Config.EnRoute = "You are on the road. You have already traveled"
+-- Waiting text
+Config.EnRoute = "You are on the way. You have already traveled"
 
--- "points" traduits en langage RP / "points" for RP purpose
-Config.PointsRP = "kilomètres"
--- Config.PointsRP = "kilometers"
+-- "points" for RP purpose
+Config.PointsRP = "Miles"
 
--- Position dans la file / position in the queue
-Config.Position = "Vous êtes en position "
--- Config.Position = "You are in position "
+-- Position in the queue
+Config.Position = "You are in position "
 
--- Texte avant les emojis / Text before emojis
-Config.EmojiMsg = "Si les emojis sont figés, relancez votre client : "
--- Config.EmojiMsg = "If the emojis are frozen, restart your client: "
+-- Text before emojis
+Config.EmojiMsg = "If the emojis are frozen, restart your client: "
 
--- Quand le type gagne à la loterie / When the player win the lottery
-Config.EmojiBoost = "!!! Youpi, " .. Config.LoterieBonusPoints .. " " .. Config.PointsRP .. " gagnés !!!"
--- Config.EmojiBoost = "!!! Yippee, " .. Config.LoterieBonusPoints .. " " .. Config.PointsRP .. " won !!!"
+-- When the player win the lottery
+Config.EmojiBoost = "!!! Yippee, " .. Config.LoterieBonusPoints .. " " .. Config.PointsRP .. " won !!!"
 
 -- Anti-spam message / anti-spam text
-Config.PleaseWait_1 = "Veuillez patienter "
-Config.PleaseWait_2 = " secondes. La connexion se lancera automatiquement !"
--- Config.PleaseWait_1 = "Please wait "
--- Config.PleaseWait_2 = " seconds. The connection will start automatically!"
+Config.PleaseWait_1 = "Please wait "
+Config.PleaseWait_2 = " seconds. The connection will start automatically!"
 
--- Me devrait jamais s'afficher / Should never be displayed
-Config.Accident = "Oups, vous venez d'avoir un accident... Si cela se reproduit, vous pouvez en informer le support :)"
--- Config.Accident = "Oops, you just had an accident ... If it happens again, you can inform the support :)"
+-- Should never be displayed / accident message
+Config.Accident = "Oops, you just had an accident ... If it happens again, contact support :)"
 
--- En cas de points négatifs / In case of negative points
-Config.Error = " ERREUR : RELANCEZ LA ROCADE ET CONTACTEZ LE SUPPORT DU SERVEUR "
--- Config.Error = " ERROR : RESTART THE QUEUE SYSTEM AND CONTACT THE SUPPORT "
+--In case of negative points
+Config.Error = " ERROR : RESTART THE QUEUE SYSTEM AND CONTACT SUPPORT "
 
 
 Config.EmojiList = {
